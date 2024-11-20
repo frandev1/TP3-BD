05bc421cecc6590a087a60a37e96b71a3f7fd3f8
Franco Rojas <fvrl.cr@gmail.com>
2024-11-20 03:51:14 -0600

    Falta corregir nuevo vin y fechas

--- database/Trigger.sql
+++ database/Trigger.sql
@@ -91,7 +91,7 @@ BEGIN
 END
 GO
 
-ALTER TRIGGER [dbo].[trUpdateTF]
+CREATE TRIGGER [dbo].[trUpdateTF]
 ON dbo.TF
 AFTER UPDATE
 AS
@@ -100,144 +100,132 @@ BEGIN
 
     -- Declaración de variables
     DECLARE @CodigoTC VARCHAR(64);
-    DECLARE @NuevoVIN BIGINT; -- El nuevo VIN será un número generado aleatoriamente
+    DECLARE @NuevoVIN VARCHAR(16); -- El nuevo VIN será un número generado aleatoriamente
     DECLARE @TipoTC VARCHAR(64);
-	DECLARE @idTCM INT;			-- Se declara idTCM en caso de que la TF sea de una TCM
+    DECLARE @idTCM INT; -- ID del TCM si aplica
     DECLARE @CantidadAnos FLOAT;
     DECLARE @NuevaFechaVencimiento DATE;
     DECLARE @NuevoCCV VARCHAR(4);
+    DECLARE @Largo INT;
+    DECLARE @Contador INT;
 
-    -- Obtener el código de tarjeta actualizado
+    -- Crear una tabla temporal para las tarjetas inactivadas
+    DECLARE @TarjetasInactivadas TABLE (
+        id INT IDENTITY(1,1),
+        CodigoTC VARCHAR(64),
+        TipoTC VARCHAR(64)
+    );
+
+    -- Poblar la tabla temporal con las tarjetas afectadas
+    INSERT INTO @TarjetasInactivadas (CodigoTC, TipoTC)
     SELECT 
-        @CodigoTC = U.CodigoTC
-    FROM inserted U;
+        I.CodigoTC,
+        CASE
+            WHEN TCA.id IS NOT NULL THEN 'TCA'
+            WHEN TCM.id IS NOT NULL THEN 'TCM'
+            ELSE NULL
+        END AS TipoTC
+    FROM inserted I
+    LEFT JOIN TCA ON I.CodigoTC = TCA.Codigo
+    LEFT JOIN TCM ON I.CodigoTC = TCM.Codigo
+    WHERE I.EsActiva = 0; -- Solo procesar tarjetas que fueron inactivadas
 
-    -- Determinar el tipo de tarjeta
-    SELECT
-        @TipoTC =
-            CASE
-                WHEN TCA.id IS NOT NULL THEN 'TCA'
-                WHEN TCM.id IS NOT NULL THEN 'TCM'
-                ELSE NULL
-            END
-    FROM [sistemaTarjetaCredito].[dbo].[TF] TF
-    LEFT JOIN TCA TCA ON TF.CodigoTC = TCA.Codigo
-    LEFT JOIN TCM TCM ON TF.CodigoTC = TCM.Codigo
-    WHERE TF.CodigoTC = @CodigoTC;
+    -- Obtener el número de filas en la tabla temporal
+    SELECT @Largo = COUNT(*) FROM @TarjetasInactivadas;
 
-    -- Solo actuar si la tarjeta fue inactivada
-    IF EXISTS (
-        SELECT 1 
-        FROM inserted U
-        WHERE U.CodigoTC = @CodigoTC AND U.EsActiva = 0
-    )
+    -- Inicializar el contador
+    SET @Contador = 1;
+
+    -- Procesar cada tarjeta inactivada
+    WHILE @Contador <= @Largo
     BEGIN
-		IF @TipoTC = 'TCM'
-		BEGIN
+        -- Obtener los datos de la tarjeta actual
+        SELECT 
+            @CodigoTC = CodigoTC,
+            @TipoTC = TipoTC
+        FROM @TarjetasInactivadas
+        WHERE id = @Contador;
 
-			--Obtener el id del TCM
-			SELECT
-				@idTCM = TCM.id
-			FROM [sistemaTarjetaCredito].[dbo].[TF] TF
-			LEFT JOIN TCM TCM ON TF.CodigoTC = TCM.Codigo
-			WHERE TF.CodigoTC = @CodigoTC;
+        -- Validar si es TCM o TCA
+        IF @TipoTC = 'TCM'
+        BEGIN
+            -- Obtener el idTCM
+            SELECT @idTCM = TCM.id
+            FROM TCM
+            WHERE TCM.Codigo = @CodigoTC;
 
-			-- Obtener la cantidad de años para la nueva fecha de vencimiento
-			SELECT @CantidadAnos = RN.Valor
-			FROM RN RN
-			WHERE RN.Nombre = 'Cantidad de Años para Vencimiento de TF'
-			AND RN.idTTCM = (
-				SELECT idTTCM
-				FROM TCM
-				WHERE id = @idTCM
-			);
+            -- Obtener la cantidad de años para el vencimiento
+            SELECT @CantidadAnos = RN.Valor
+            FROM RN
+            WHERE RN.Nombre = 'Cantidad de Años para Vencimiento de TF'
+              AND RN.idTTCM = (
+                  SELECT idTTCM
+                  FROM TCM
+                  WHERE id = @idTCM
+              );
 
-			-- Calcular la nueva fecha de vencimiento
-			SET @NuevaFechaVencimiento = DATEADD(YEAR, @CantidadAnos, GETDATE());
+            -- Calcular la nueva fecha de vencimiento
+            SET @NuevaFechaVencimiento = DATEADD(YEAR, @CantidadAnos, GETDATE());
 
-			-- Generar un nuevo VIN como un número aleatorio único
-			SET @NuevoVIN = ABS(CHECKSUM(NEWID()) % 10000000000000000);
+            SET @NuevoVIN = RIGHT('0000000000000000' + CAST((ABS(CHECKSUM(NEWID())) % 10000000000000000) AS VARCHAR), 16);
 
-			-- Validar que el nuevo VIN no exista ya en la tabla
-			WHILE EXISTS (SELECT 1 FROM dbo.TF WHERE Codigo = @NuevoVIN)
-			BEGIN
-				SET @NuevoVIN = ABS(CHECKSUM(NEWID()) % 10000000000000000);
-			END;
+            WHILE EXISTS (SELECT 1 FROM TF WHERE Codigo = @NuevoVIN)
+            BEGIN
+				SET @NuevoVIN = RIGHT('0000000000000000' + CAST((ABS(CHECKSUM(NEWID())) % 10000000000000000) AS VARCHAR), 16);
+            END;
 
-			-- Generar un nuevo CCV aleatorio (3 dígitos)
-			SET @NuevoCCV = RIGHT('000' + CAST((ABS(CHECKSUM(NEWID())) % 1000) AS VARCHAR), 3);
+            -- Generar un nuevo CCV aleatorio (4 dígitos)
+            SET @NuevoCCV = RIGHT('0000' + CAST((ABS(CHECKSUM(NEWID())) % 10000) AS VARCHAR), 4);
 
-			-- Insertar la nueva tarjeta física en la tabla TF
-			INSERT INTO [dbo].[TF]
-			([Codigo]
-			,[CodigoTC]
-			,[FechaVencimiento]
-			,[CCV]
-			,[EsActiva]
-			,[FechaCreacion]
-			,[idMotivoInvalidacion])
-			VALUES
-			(@NuevoVIN,             -- Nuevo VIN generado
-				@CodigoTC,            -- Código de la tarjeta asociada
-				@NuevaFechaVencimiento, -- Nueva fecha de vencimiento
-				@NuevoCCV,            -- Nuevo CCV
-				1,                    -- Activar la tarjeta
-				GETDATE(),            -- Fecha de creación actual
-				NULL);                -- Sin motivo de inactivación
-		END
-		ELSE
-		BEGIN
-			--Obtener el id del TCM
-			SELECT
-				@idTCM = TCA.idTCM
-			FROM [sistemaTarjetaCredito].[dbo].[TF] TF
-			LEFT JOIN TCA TCA ON TF.CodigoTC = TCA.Codigo
-			WHERE TF.CodigoTC = @CodigoTC;
+            -- Insertar la nueva tarjeta física
+            INSERT INTO TF (Codigo, CodigoTC, FechaVencimiento, CCV, EsActiva, FechaCreacion, idMotivoInvalidacion)
+            VALUES
+                (@NuevoVIN, @CodigoTC, @NuevaFechaVencimiento, @NuevoCCV, 1, GETDATE(), NULL);
+        END
+        ELSE IF @TipoTC = 'TCA'
+        BEGIN
+            -- Obtener el idTCM asociado a la TCA
+            SELECT @idTCM = TCA.idTCM
+            FROM TCA
+            WHERE TCA.Codigo = @CodigoTC;
 
-			-- Obtener la cantidad de años para la nueva fecha de vencimiento
-			SELECT @CantidadAnos = RN.Valor
-			FROM RN RN
-			WHERE RN.Nombre = 'Cantidad de Años para Vencimiento de TF'
-			AND RN.idTTCM = (
-				SELECT idTTCM
-				FROM TCM
-				WHERE id = @idTCM
-			)
-			;
+            -- Obtener la cantidad de años para el vencimiento
+            SELECT @CantidadAnos = RN.Valor
+            FROM RN
+            WHERE RN.Nombre = 'Cantidad de Años para Vencimiento de TF'
+              AND RN.idTTCM = (
+                  SELECT idTTCM
+                  FROM TCM
+                  WHERE id = @idTCM
+              );
 
-			-- Calcular la nueva fecha de vencimiento
-			SET @NuevaFechaVencimiento = DATEADD(YEAR, @CantidadAnos, GETDATE());
+            -- Calcular la nueva fecha de vencimiento
+            SET @NuevaFechaVencimiento = DATEADD(YEAR, @CantidadAnos, GETDATE());
 
-			-- Generar un nuevo VIN como un número aleatorio único
-			SET @NuevoVIN = ABS(CHECKSUM(NEWID()) % 10000000000000000);
+            -- Generar un nuevo VIN único
+            SET @NuevoVIN = CAST(ABS(CHECKSUM(NEWID())) AS BIGINT) 
+                          + CAST(ABS(CHECKSUM(NEWID())) % 1000000000000 AS BIGINT);
 
-			-- Validar que el nuevo VIN no exista ya en la tabla
-			WHILE EXISTS (SELECT 1 FROM dbo.TF WHERE Codigo = @NuevoVIN)
-			BEGIN
-				SET @NuevoVIN = ABS(CHECKSUM(NEWID()) % 10000000000000000);
-			END;
+            SET @NuevoVIN = RIGHT('0000000000000000' + CAST(@NuevoVIN AS VARCHAR), 16);
 
-			-- Generar un nuevo CCV aleatorio (3 dígitos)
-			SET @NuevoCCV = RIGHT('000' + CAST((ABS(CHECKSUM(NEWID())) % 1000) AS VARCHAR), 3);
+            WHILE EXISTS (SELECT 1 FROM TF WHERE Codigo = @NuevoVIN)
+            BEGIN
+                SET @NuevoVIN = CAST(ABS(CHECKSUM(NEWID())) AS BIGINT) 
+                              + CAST(ABS(CHECKSUM(NEWID())) % 1000000000000 AS BIGINT);
+                SET @NuevoVIN = RIGHT('0000000000000000' + CAST(@NuevoVIN AS VARCHAR), 16);
+            END;
 
-			-- Insertar la nueva tarjeta física en la tabla TF
-			INSERT INTO [dbo].[TF]
-			([Codigo]
-			,[CodigoTC]
-			,[FechaVencimiento]
-			,[CCV]
-			,[EsActiva]
-			,[FechaCreacion]
-			,[idMotivoInvalidacion])
-			VALUES
-			(@NuevoVIN,             -- Nuevo VIN generado
-			@CodigoTC,            -- Código de la tarjeta asociada
-			@NuevaFechaVencimiento, -- Nueva fecha de vencimiento
-			@NuevoCCV,            -- Nuevo CCV
-			1,                    -- Activar la tarjeta
-			GETDATE(),            -- Fecha de creación actual
-			NULL);                -- Sin motivo de inactivación
-		END
+            -- Generar un nuevo CCV aleatorio (3 dígitos)
+            SET @NuevoCCV = RIGHT('0000' + CAST((ABS(CHECKSUM(NEWID())) % 1000) AS VARCHAR), 4);
+
+            -- Insertar la nueva tarjeta física
+            INSERT INTO TF (Codigo, CodigoTC, FechaVencimiento, CCV, EsActiva, FechaCreacion, idMotivoInvalidacion)
+            VALUES
+                (@NuevoVIN, @CodigoTC, @NuevaFechaVencimiento, @NuevoCCV, 1, GETDATE(), NULL);
+        END
+
+        -- Incrementar el contador
+        SET @Contador = @Contador + 1;
     END
 END;
 GO
